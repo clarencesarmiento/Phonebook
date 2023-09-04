e088a341a09d9f548645feea79070988b1fd890c
Clarence Sarmiento <clarencesarmiento1020@gmail.com>
2023-09-04 19:27:00 +0800

    updates

--- main.py
+++ main.py
@@ -1,183 +1,204 @@
+import customtkinter as ctk
+from CTkMessagebox import CTkMessagebox
 from tkinter import *
-from tkinter import ttk, messagebox
-from PIL import ImageTk
-import mysql.connector
-from mysql.connector import errorcode
-import sys
+from tkinter import ttk
+from PIL import Image
+import pyodbc
 import re
 
+ctk.set_appearance_mode('dark')
+ctk.set_default_color_theme('green')
 
-class Window:
-    def __init__(self, root, db_name, conn, cursor):
-        self.root = root
-        self.db_name = db_name
+appWidth, appHeight = 600, 300
+
+
+class EntryFrame(ctk.CTkFrame):
+    def __init__(self, master, conn, **kwargs):
+        super().__init__(master, **kwargs)
         self.conn = conn
-        self.cursor = cursor
+        self.cursor = self.conn.cursor()
 
-        # Create Window
-        self.root.title('Phonebook')
-        self.root.iconbitmap('assets/phonebook-9.ico')
-        self.root.geometry('700x400')
-        self.root.resizable(False, False)
+        # Frame Title
+        self.entryframe_title = ctk.CTkLabel(self, text='INFORMATION', font=('helvetica', 25, 'bold'),
+                                             text_color='white')
+        self.entryframe_title.grid(row=0, column=0, padx=10, pady=(15, 0), columnspan=2, sticky='nsew')
 
-        # Entry Frame
-        self.entry_frame = Frame(self.root, bg='gray')
-        self.entry_frame.place(x=10, y=10, width=300, height=380)
-        self.entry_frame_label = Label(self.entry_frame, text='Contacts', font=('norwester', 30), bg='gray')
-        self.entry_frame_label.place(x=65, y=5)
+        # Frame Icon
+        self.man_icon = ctk.CTkImage(Image.open('assets/dark_man.png'), size=(64, 64))
+        self.man_icon_label = ctk.CTkLabel(self, image=self.man_icon, text='', anchor='center')
+        self.man_icon_label.grid(row=1, column=0, padx=8, pady=(10, 0), columnspan=2, sticky='ew')
 
-        self.man_icon = ImageTk.PhotoImage(file='assets/light_man.png')
-        self.man_icon_label = Label(self.entry_frame, image=self.man_icon, bg='gray')
-        self.man_icon_label.place(x=25, y=110)
+        # Frame Labels
+        self.firstname_label = ctk.CTkLabel(self, text='First Name', font=('helvetica', 15), text_color='white',
+                                            anchor='center')
+        self.firstname_label.grid(row=2, column=0, padx=10, pady=(10, 0), sticky='ew')
 
-        self.phone_icon = ImageTk.PhotoImage(file='assets/phone.png')
-        self.phone_icon_label = Label(self.entry_frame, image=self.phone_icon, bg='gray')
-        self.phone_icon_label.place(x=25, y=205)
+        self.lastname_label = ctk.CTkLabel(self, text='Last Name', font=('helvetica', 15), text_color='white',
+                                           anchor='center')
+        self.lastname_label.grid(row=3, column=0, padx=10, pady=(10, 0), sticky='ew')
 
-        # Contact Information
-        self.firstname_label = Label(self.entry_frame, text='First Name', font=('RobotoCondensed-Light', 12, 'bold'),
-                                     bg='gray')
-        self.firstname_label.place(x=100, y=70)
-        self.user_firstname = Entry(self.entry_frame, font=('RobotoCondensed-Light', 15), bg='white', width=15)
-        self.user_firstname.place(x=102, y=95)
+        self.phone_number_label = ctk.CTkLabel(self, text='Phone Number', font=('helvetica', 15), text_color='white',
+                                               anchor='w')
+        self.phone_number_label.grid(row=4, column=0, padx=10, pady=(10, 0), sticky='ew')
 
-        self.lastname_label = Label(self.entry_frame, text='Last Name', font=('RobotoCondensed-Light', 12, 'bold'),
-                                    bg='gray')
-        self.lastname_label.place(x=100, y=130)
-        self.user_lastname = Entry(self.entry_frame, font=('RobotoCondensed-Light', 15), bg='white', width=15)
-        self.user_lastname.place(x=102, y=155)
+        # Frame Entries
+        self.user_firstname = ctk.CTkEntry(self, placeholder_text='Enter First Name')
+        self.user_firstname.grid(row=2, column=1, padx=10, pady=(10, 0), sticky='ew')
 
-        self.phone_number_label = Label(self.entry_frame, text='Phone Number',
-                                        font=('RobotoCondensed-Light', 12, 'bold'), bg='gray')
-        self.phone_number_label.place(x=100, y=200)
-        self.user_phone_number = Entry(self.entry_frame, font=('RobotoCondensed-Light', 15), bg='white', width=15)
-        self.user_phone_number.place(x=102, y=225)
+        self.user_lastname = ctk.CTkEntry(self, placeholder_text='Enter Last Name')
+        self.user_lastname.grid(row=3, column=1, padx=10, pady=(10, 0), sticky='ew')
 
-        # Database frame
-        self.db_frame = Frame(self.root, bg='lightgray')
-        self.db_frame.place(x=320, y=10, width=370, height=380)
-        self.db_view = ttk.Treeview(self.db_frame)
-        self.db_view['columns'] = ("First Name", "Last Name", "Phone Number")
-        self.db_view.column("#0", width=0, stretch=False)
-        self.db_view.column("First Name", anchor=W, width=116)
-        self.db_view.column("Last Name", anchor=W, width=116)
-        self.db_view.column("Phone Number", anchor=CENTER, width=116)
+        self.user_phonenumber = ctk.CTkEntry(self, placeholder_text='Enter Phone Number')
+        self.user_phonenumber.grid(row=4, column=1, padx=10, pady=(10, 0), sticky='ew')
 
-        # Create Headings
-        self.db_view.heading("First Name", text="First Name", anchor=W)
-        self.db_view.heading("Last Name", text="Last Name", anchor=W)
-        self.db_view.heading("Phone Number", text="Phone Number", anchor=CENTER)
-        self.db_view.place(x=10, y=10)
+        # Frame Buttons
+        self.info_button = ctk.CTkButton(self, text='i', font=('times new roman', 15, 'bold'), width=2,
+                                         corner_radius=15, fg_color='transparent', border_color='#AEC3AE',
+                                         border_width=1, anchor='e')
+        self.info_button.grid(row=0, column=1, padx=5, pady=5, sticky='ne')
 
-        self.cursor.execute('SELECT * FROM contacts')
-        for row in self.cursor.fetchall():
-            self.db_view.insert("", "end", values=(row[0], row[1], row[2]))
+        self.add_button = ctk.CTkButton(self, text='Add', cursor='hand2', command=self.add_contact, width=80)
+        self.add_button.grid(row=5, column=0, padx=10, pady=(15, 10), sticky='w')
+        self.add_button.bind('<Return>', self.add_contact)
 
-        # Buttons
-        self.add_contact_button = Button(self.entry_frame, command=self.add_contact, cursor='hand2', text='Add',
-                                         bg='lightgray',
-                                         font=('RobotoCondensed-Light', 10, 'bold'), width=10)
-        self.add_contact_button.place(x=10, y=300)
-        self.update_contact_button = Button(self.entry_frame, cursor='hand2', text='Update', bg='lightgray',
-                                            font=('RobotoCondensed-Light', 10, 'bold'), width=10)
-        self.update_contact_button.place(x=105, y=300)
-        self.delete_contact_button = Button(self.entry_frame, cursor='hand2', text='Delete', bg='lightgray',
-                                            font=('RobotoCondensed-Light', 10, 'bold'), width=10)
-        self.delete_contact_button.place(x=200, y=300)
-        # self.database_data = Button(self.entry_frame, command=self.load_database, cursor='hand2',
-        #                                   text='Load Database', bg='lightgray',
-        #                                   font=('RobotoCondensed-Light', 8, 'bold'))
-        # self.database_data.place(x=10, y=350)
+        self.update_button = ctk.CTkButton(self, text='Update', cursor='hand2', width=80)
+        self.update_button.grid(row=5, column=0, columnspan=2, padx=10, pady=(15, 10))
 
-    def get_data(self):
-        user_firstname = self.user_firstname.get()
-        user_lastname = self.user_lastname.get()
-        user_phone_number = self.user_phone_number.get()
+        self.delete_button = ctk.CTkButton(self, text='Delete', cursor='hand2', width=80)
+        self.delete_button.grid(row=5, column=1, padx=10, pady=(15, 10), sticky='e')
 
-        return user_firstname.strip().title(), user_lastname.strip().title(), user_phone_number.strip()
+    def get_entry_data(self):
+        return self.user_firstname.get().strip().title(), self.user_lastname.get().strip().title(), self.user_phonenumber.get().strip().title()
 
     def clear_entrybox(self):
         self.user_firstname.delete(0, 'end')
         self.user_lastname.delete(0, 'end')
-        self.user_phone_number.delete(0, 'end')
+        self.user_phonenumber.delete(0, 'end')
 
-    def add_contact(self):
-        phone_number_format = r"^(9[0-9]{9})$"
-        user_firstname, user_lastname, user_phone_number = self.get_data()
-        if user_firstname and user_lastname and user_phone_number != '':
-            if re.search(phone_number_format, user_phone_number):
+    def add_contact(self, event=None):
+        phone_number_format = r"^(0?9[0-9]{9})$"
+        user_firstname, user_lastname, user_phonenumber = self.get_entry_data()
+        if user_firstname and user_lastname and user_phonenumber != '':
+            if re.search(phone_number_format, user_phonenumber):
                 self.cursor.execute('SELECT * FROM contacts')
                 for row in self.cursor.fetchall():
                     first_name, last_name, phone_number = row[0], row[1], row[2]
-                    if user_firstname == first_name and user_lastname == last_name and user_phone_number == phone_number:
-                        messagebox.showinfo(title='Add Contact', message='Contact Already Added.')
+                    if user_firstname == first_name and user_lastname == last_name and user_phonenumber == phone_number:
                         break
                 else:
-                    addcontact = ("INSERT INTO contacts"
-                                  "(first_name, last_name, phone_number)"
-                                  "VALUES (%s, %s, %s)")
-                    datacontact = (user_firstname, user_lastname, user_phone_number)
-                    self.cursor.execute(addcontact, datacontact)
+                    query = ("INSERT INTO contacts"
+                             "(FirstName, LastName, PhoneNumber)"
+                             "VALUES (?, ?, ?)")
+                    datacontact = (user_firstname, user_lastname, user_phonenumber)
+                    self.cursor.execute(query, datacontact)
                     self.conn.commit()
-                    self.db_view.insert("", "end", values=(user_firstname, user_lastname, user_phone_number))
-                    messagebox.showinfo(title='Add Contact', message='Contact Added Successfully.')
-
+                    CTkMessagebox(title='Add Contact', message='Contact Added Successfully.', icon='check',
+                                  option_1='Thanks')
+                self.clear_entrybox()
             else:
-                print('Invalid Phone Number')
+                CTkMessagebox(title='Error', message='Wrong Phone number format.', icon='cancel', sound=True)
+                self.user_phonenumber.delete(0, 'end')
         else:
-            messagebox.showerror(title='Add Contact', message='All fields are Required.', parent=self.root)
+            CTkMessagebox(title='Warning', message='All fields are Required.', icon='warning', sound=True)
 
-        self.clear_entrybox()
 
-    def update_contact(self):
-        pass
+class DataFrame(ctk.CTkFrame):
+    def __init__(self, master, conn, **kwargs):
+        super().__init__(master, **kwargs)
+        self.conn = conn
+        self.cursor = self.conn.cursor()
 
-    def delete_contact(self):
-        pass
+        # Define StringVar
+        self.to_search = StringVar()
 
-    def view_database(self):
-        pass
+        # Frame Title
+        self.dataframe_title = ctk.CTkLabel(self, text='RECORD', font=('helvetica', 25, 'bold'), text_color='white')
+        self.dataframe_title.grid(row=0, column=0, padx=10, pady=(15, 0), columnspan=3, sticky='ew')
 
-    # def load_database(self):
-    #
-    #     if len(self.current_data) == 0:
-    #         for row in self.cursor.fetchall():
-    #             self.db_view.insert("", "end", values=(row[0], row[1], row[2]))
-    #             self.current_data.append((row[0], row[1], row[2]))
+        # Frame Entries
+        self.to_search = ctk.CTkEntry(self, placeholder_text='Search')
+        self.to_search.grid(row=1, column=0, padx=(10, 0), pady=(10, 0), sticky='ew')
+
+        # Buttons
+        self.search_button = ctk.CTkButton(self, text='Search', cursor='hand2', command=self.search, width=60)
+        self.search_button.grid(row=1, column=1, padx=(10, 0), pady=(10, 0), sticky='ew')
+
+        self.clear_button = ctk.CTkButton(self, text='Clear', cursor='hand2', width=40)
+        self.clear_button.grid(row=1, column=2, padx=10, pady=(10, 0), sticky='ew')
+
+        self.style = ttk.Style()
+        self.style.configure("mystyle.Treeview.Heading", font=('helvetica', 15, 'bold'))
+        self.style.configure("mystyle.Treeview", highlightthickness=0, bd=0, font=('Calibri', 15))
+        self.db_view = ttk.Treeview(self, columns=("firstname", "lastname", "phonenumber"), show='headings',
+                                    style='mystyle.Treeview')
+
+        # Create Headings
+        self.db_view.heading("firstname", text="First Name", anchor='w')
+        self.db_view.heading("lastname", text="Last Name", anchor='w')
+        self.db_view.heading("phonenumber", text="Phone Number", anchor='w')
+        self.db_view.grid(row=2, column=0, padx=15, pady=15, columnspan=3, sticky='nsew')
+
+        self.update_dataframe()
+
+    def search(self):
+        to_search = self.to_search.get()
+        query = 'SELECT FirstName, LastName, PhoneNumber FROM contacts WHERE '
+
+    def update_dataframe(self):
+        self.cursor.execute('SELECT * FROM contacts')
+        for row in sorted(self.cursor.fetchall()):
+            first_name, last_name, phone_number = row[0], row[1], row[2]
+            self.db_view.insert(parent='', index='end', values=(first_name, last_name, phone_number))
+
+    # def insert_data(self, firstname, lastname, phonenumber):
+    #     record = (firstname, lastname, phonenumber)
+    #     if record in self.data:
+    #         print('Already in Record.')
     #     else:
-    #         messagebox.showinfo(title='Load Database', message='All data from Database has been loaded.')
+    #         self.db_view.insert(parent='', index=0, values=record)
+    #         # self.data.append(record)
 
+
+class App(ctk.CTk):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        config = (
+            r'DRIVER={Microsoft Access Driver (*.mdb, *.accdb)};'
+            r'DBQ=C:\Users\acer\PycharmProjects\Phonebook\phonebook.accdb'
+        )
+        self.conn = pyodbc.connect(config)
+
+        # self.cursor = database.cursor()
+        # Check Database connection
+        # for table_info in self.cursor.tables(tableType='TABLE'):
+        #     print(table_info.table_name)
+
+        # Create Window
+        self.title('Phonebook')
+        self.iconbitmap('assets/telephone-directory.ico')
+        # self.geometry(f"{appWidth}x{appHeight}")
+        self.minsize(appWidth, appHeight)
+        self.resizable(False, False)
+        self.configure(fg_color='#040D12')
+        self.columnconfigure(0, weight=1)
+
+        self.data_frame = DataFrame(master=self, conn=self.conn, fg_color='#5C8374', border_color='white',
+                                    border_width=2, width=300, height=300)
+        self.data_frame.grid(row=0, column=1, padx=(0, 10), pady=10, sticky='nsew')
+        self.data_frame.columnconfigure(0, weight=1)
+        self.data_frame.rowconfigure(2, weight=1)
+
+        self.entry_frame = EntryFrame(master=self, conn=self.conn, fg_color='#183D3D',
+                                      border_color='#AEC3AE', border_width=2,
+                                      width=300, height=300)
+
+        self.entry_frame.grid(row=0, column=0, padx=10, pady=10, sticky='nsew')
+        self.entry_frame.columnconfigure(0, weight=1)
 
 
 def main():
-    database = 'phonebook'
-    config = {
-        'host': 'localhost',
-        'port': '3306',
-        'user': 'root',
-        'password': 'Sarmiento1020',
-        'database': database
-    }
-
-    try:
-        conn = mysql.connector.connect(**config)
-        if conn.is_connected():
-            db = conn.get_server_info()
-            # messagebox.showinfo(title='Database Connection', message=f'Connected to MySQL Server {db}')
-            cursor = conn.cursor()
-            root = Tk()
-            window = Window(root, database, conn, cursor)
-            root.mainloop()
-    except mysql.connector.Error as err:
-        if err.errno == errorcode.ER_ACCESS_DENIED_ERROR:
-            messagebox.showerror(title='Connection Access Denied',
-                                 message='Something is wrong with your user name or password.')
-        elif err.errno == errorcode.ER_BAD_DB_ERROR:
-            messagebox.showerror(title='Database Error', message='Database does not exist.')
-        else:
-            print(err)
-    else:
-        conn.close()
+    app = App()
+    app.mainloop()
 
 
 if __name__ == '__main__':
